8f300bb27d0e3be626898ebb204c24aa29ead975
Takayuki ORITO <iori.ayane@gmail.com>
2019-09-01 17:30:40 +0900

    コメント追加

--- src/PostApp/fakesns4q.cpp
+++ src/PostApp/fakesns4q.cpp
@@ -5,8 +5,9 @@
 
 FakeSns4Q::FakeSns4Q(QObject *parent) : QObject(parent)
 {
+  //ユーザー情報を取得できるまでのダミー画像
   setUserIcon("noimage.jpg");
-
+  //リクエストに対するレスポンスを受け取るためのシグナル・スロットを接続
   connect(&m_networkManager, &QNetworkAccessManager::finished
           , this, &FakeSns4Q::networkMangerfinished);
 }
@@ -34,7 +35,8 @@ void FakeSns4Q::setUserId(QString userId)
   m_userId = userId;
   emit userIdChanged(m_userId);
 
-  QUrl api = "https://qwa.relog.tech/sns/user.php?id=" + userId;
+  //ユーザーIDが変更されたら要求
+  QUrl api = "https://qwa.relog.tech/sns/user?id=" + userId;
   m_networkManager.get(QNetworkRequest(api));
 }
 
@@ -58,10 +60,12 @@ void FakeSns4Q::setUserIcon(QString userIcon)
 
 void FakeSns4Q::networkMangerfinished(QNetworkReply *reply)
 {
+  //受け取った内容を処理
   QByteArray data = reply->readAll();
   qDebug() << reply->url().toString();
   qDebug() << QString(data);
 
+  //JSONをパースしてプロパティに設定
   QJsonDocument doc(QJsonDocument::fromJson(data));
   QJsonObject obj(doc.object());
 
--- src/PostApp/fakesns4q.h
+++ src/PostApp/fakesns4q.h
@@ -9,26 +9,34 @@ class FakeSns4Q : public QObject
 {
   Q_OBJECT
 
-  Q_PROPERTY(QString userId READ userId WRITE setUserId NOTIFY userIdChanged)
-  Q_PROPERTY(QString userName READ userName WRITE setUserName NOTIFY userNameChanged)
-  Q_PROPERTY(QString userIcon READ userIcon WRITE setUserIcon NOTIFY userIconChanged)
+  //QMLに公開するプロパティ
+  Q_PROPERTY(QString userId READ userId WRITE setUserId
+             NOTIFY userIdChanged)
+  Q_PROPERTY(QString userName READ userName WRITE setUserName
+             NOTIFY userNameChanged)
+  Q_PROPERTY(QString userIcon READ userIcon WRITE setUserIcon
+             NOTIFY userIconChanged)
 
 public:
   explicit FakeSns4Q(QObject *parent = nullptr);
 
+  //プロパティのゲッター
   QString userId() const;
   QString userName() const;
   QString userIcon() const;
 
 signals:
+  //プロパティの値が変化したことを通知するシグナル
   void userIdChanged(QString userId);
   void userNameChanged(QString userName);
   void userIconChanged(QString userIcon);
 
 public slots:
+  //プロパティのセッター
   void setUserId(QString userId);
   void setUserName(QString userName);
   void setUserIcon(QString userIcon);
+  //リクエストに対するレスポンスを受け取るためのスロット
   void networkMangerfinished(QNetworkReply *reply);
 
 private:
@@ -37,8 +45,6 @@ private:
   QString m_userIcon;
 
   QNetworkAccessManager m_networkManager;
-
-
 };
 
 #endif // FAKESNS4Q_H
