9395ae1e8a0b2f35afe3267d9fff875cc83999aa
Takayuki ORITO <iori.ayane@gmail.com>
2019-09-01 13:36:56 +0900

    MIME修正

--- src/PostAppWeb/user.php
+++ src/PostAppWeb/user.php
@@ -1,5 +1,5 @@
 <?php
-  header("Content-type: text/html; charset=utf-8");
+  header("Content-type: application/json; charset=utf-8");
   header("Access-Control-Allow-Origin: *");
 
   $id = $_GET["id"];
